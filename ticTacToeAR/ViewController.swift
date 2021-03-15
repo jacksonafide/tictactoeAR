4dac5baa20b71a9e8d916a7534ab6e78c4e2c589
Jacek Chojnacki <jacek.0411@gmail.com>
2021-03-15 23:08:12 +0100

    Initial Commit

---
+++ ticTacToeAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ticTacToeAR
+//
+//  Created by Jacek Chojnacki on 15/03/2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
